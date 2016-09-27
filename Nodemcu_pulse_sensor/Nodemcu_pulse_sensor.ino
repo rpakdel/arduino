36bdd8b7f9f0eaa1c6fee34db666f16231d8138a
Reza Pakdel <rpakdel@gmail.com>
2016-09-26 22:19:52 -0700

    Nodemcu with pulse heart rate sensor

---
+++ Nodemcu_pulse_sensor/Nodemcu_pulse_sensor.ino
@@ -0,0 +1,59 @@
+
+/*  Pulse Sensor Amped 1.4    by Joel Murphy and Yury Gitman   http://www.pulsesensor.com
+
+----------------------  Notes ----------------------  ---------------------- 
+This code:
+1) Blinks an LED to User's Live Heartbeat   PIN 13
+2) Fades an LED to User's Live HeartBeat
+3) Determines BPM
+4) Prints All of the Above to Serial
+
+Read Me:
+https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/README.md   
+ ----------------------       ----------------------  ----------------------
+*/
+
+//  Variables
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded! 
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat". 
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+// Regards Serial OutPut  -- Set This Up to your needs
+static boolean serialVisual = true;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse 
+
+void setup()
+{
+  
+  Serial.begin(115200);             // we agree to talk fast!
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS 
+   // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE, 
+   // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
+//   analogReference(EXTERNAL);   
+}
+
+
+//  Where the Magic Happens
+void loop(){
+  
+    serialOutput() ;       
+    
+  if (QS == true){     // A Heartbeat Was Found
+                       // BPM and IBI have been Determined
+                       // Quantified Self "QS" true when arduino finds a heartbeat        
+        serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.     
+        QS = false;                      // reset the Quantified Self flag for next time    
+  }
+  delay(20);                             //  take a break
+}
+
+
+
+
+
+
+
---
+++ Nodemcu_pulse_sensor/readme.md
@@ -0,0 +1,2 @@
+Nodemcu with pulse heart rate sensor basic working example
+(Not sure if the interrupt is setup correctly)

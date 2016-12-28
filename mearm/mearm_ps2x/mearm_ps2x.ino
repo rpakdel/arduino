810086a03b139c4b16c599d759026b5ed66c077d
Reza Pakdel <rpakdel@gmail.com>
2016-12-28 00:13:12 -0800

    ignore Libraries

--- mearm/mearm_ps2x/mearm_ps2x.ino
+++ mearm/mearm_ps2x/mearm_ps2x.ino
@@ -16,10 +16,16 @@ PS2X ps2x;
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+// servo index
+
 #define BASE_SERVO 0  // middle
 #define LEFT_SERVO 1  // left
 #define RIGHT_SERVO 2 // right
 #define CLAW_SERVO 3  // claw
+
+// PWM controller pins
+
+uint8_t pwmPins[4] = { 0, 15, 1, 3 };
 
 #define CLAW_MIN_PULSE 450 // open
 #define CLAW_MAX_PULSE 620 // closed
@@ -27,21 +33,24 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // actual min/max [135, 585] based on BASE servo
 // try 150, 530 to make base centered
 int servo_min_pulse[4] = { 135, 200, 300, CLAW_MIN_PULSE };
-int servo_max_pulse[4] = { 585, 600, 550, CLAW_MAX_PULSE };
+int servo_max_pulse[4] = { 585, 475, 500, CLAW_MAX_PULSE };
 
-#define HISTORY 10
+#define HISTORY 40
 
 int8_t leftValues[HISTORY];
 int8_t rightValues[HISTORY];
 int8_t baseValues[HISTORY];
 int8_t clawValues[HISTORY];
 
-uint16_t currentPulseValues[4] = { 0, 0, 0, 0 };
-uint16_t prevPulseValues[4] = { 0, 0, 0, 0 };
+float currentPulseValues[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+float prevPulseValues[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+float valueScales[4] = { 5.0f, 10.0f, 10.0f, 10.0f };
+float turbo = 1.0f;
 
 uint8_t readIndex = 0;
 
-void resetHistoryValues(int8_t values[HISTORY], uint16_t resetValue)
+void resetHistoryValues(int8_t values[HISTORY], int8_t resetValue)
 {
     for (int i = 0; i < HISTORY; ++i)
     {
@@ -51,10 +60,12 @@ void resetHistoryValues(int8_t values[HISTORY], uint16_t resetValue)
 
 void resetAllValues()
 {
-    currentPulseValues[LEFT_SERVO] = (servo_min_pulse[LEFT_SERVO] + servo_max_pulse[LEFT_SERVO]) / 2;
-    currentPulseValues[RIGHT_SERVO] = (servo_min_pulse[RIGHT_SERVO] + servo_max_pulse[RIGHT_SERVO]) / 2;
-    currentPulseValues[BASE_SERVO] = (servo_min_pulse[BASE_SERVO] + servo_max_pulse[BASE_SERVO]) / 2;
-    currentPulseValues[CLAW_SERVO] = (servo_min_pulse[CLAW_SERVO] + servo_max_pulse[CLAW_SERVO]) / 2;
+    Serial.println("Resetting values");
+    Serial.println("-");
+    currentPulseValues[LEFT_SERVO] = (servo_min_pulse[LEFT_SERVO] + servo_max_pulse[LEFT_SERVO]) / 2.0f;
+    currentPulseValues[RIGHT_SERVO] = (servo_min_pulse[RIGHT_SERVO] + servo_max_pulse[RIGHT_SERVO]) / 2.0f;
+    currentPulseValues[BASE_SERVO] = (servo_min_pulse[BASE_SERVO] + servo_max_pulse[BASE_SERVO]) / 2.0f;
+    currentPulseValues[CLAW_SERVO] = (servo_min_pulse[CLAW_SERVO] + servo_max_pulse[CLAW_SERVO]) / 2.0f;
 
     resetHistoryValues(leftValues, 0);
     resetHistoryValues(rightValues, 0);
@@ -64,6 +75,7 @@ void resetAllValues()
 
 void setupPSX2()
 {
+    Serial.println("PS2X");
     int error = ps2x.config_gamepad(
         PSX2_CLOCK,
         PSX2_COMMAND,
@@ -101,6 +113,7 @@ void setupPSX2()
 
 void setup() 
 {
+    Serial.println("Init");
   Serial.begin(9600);
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
@@ -110,7 +123,7 @@ void setup()
 
 void setPulseLength(uint8_t servonum, uint16_t pulselen)
 {
-  pwm.setPWM(servonum, 0, pulselen);
+  pwm.setPWM(pwmPins[servonum], 0, pulselen);
 }
 
 void servo_setMin(uint8_t servonum)
@@ -153,41 +166,80 @@ void incrementReadIndex()
     }
 }
 
-uint16_t getAverageHistoryValue(int8_t values[HISTORY])
+int8_t getAverageHistoryValue(int8_t values[HISTORY])
 {
     float sum = 0;
     for (uint8_t i = 0; i < HISTORY; ++i)
     {
         sum += values[i];
     }
-    return (int)(sum / (HISTORY * 1.0f));
+    return (int8_t)(sum / (HISTORY * 1.0f));
 }
+
+uint8_t resetCounter = 0;
+
+
 
 void readValuesPS2X()
 {
     ps2x.read_gamepad(false, 0);
 
-    int ry = ps2x.Analog(PSS_RY);
-    int8_t right = map(ry, 0, 255, 100, -100) / 50;
-
-    uint8_t ly = ps2x.Analog(PSS_LY);
-    int8_t left = map(ly, 0, 255, -100, 100) / 50;
-
-    uint8_t lx = ps2x.Analog(PSS_LX);
-    int8_t base = map(lx, 0, 255, 100, -100) / 50;
-
-    int8_t claw = 2;
-    if (ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1))
+    if (ps2x.Button(PSB_CIRCLE))
     {
-        claw = -2;
+        resetCounter++;
+
+        if (resetCounter >= 100)
+        {
+            resetAllValues();
+            resetCounter = 0;
+        }
+    }
+    else
+    {
+        resetCounter = 0;
     }
 
-    //Serial.print(right);
+    if (ps2x.Button(PSB_TRIANGLE))
+    {
+        turbo = 3.0f;
+    } 
+    else if (ps2x.Button(PSB_CROSS))
+    {
+        turbo = 0.3f;
+    }
+    else
+    {
+        turbo = 1.0f;
+    }
+
+    int ry = ps2x.Analog(PSS_RY);
+    int8_t right = map(ry, 0, 255, 5, -6);
+
+    int rx = ps2x.Analog(PSS_RX);
+    int8_t base1 = map(rx, 0, 255, 5, -6);
+
+    uint8_t ly = ps2x.Analog(PSS_LY);
+    int8_t left = map(ly, 0, 255, -5, 6);
+
+    uint8_t lx = ps2x.Analog(PSS_LX);
+    int8_t base2 = map(lx, 0, 255, 5, -5);
+
+    int8_t claw = 0.0;
+    if (ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1))
+    {
+        claw = -5;
+    }
+    else if (ps2x.Button(PSB_L2) || ps2x.Button(PSB_R2))
+    {
+        claw = 5;
+    }
+
+    //Serial.print(base);
     //Serial.print(' ');
 
     leftValues[readIndex] = left;
     rightValues[readIndex] = right;
-    baseValues[readIndex] = base;
+    baseValues[readIndex] = base1 + base2;
     clawValues[readIndex] = claw;
     
     incrementReadIndex();
@@ -207,36 +259,55 @@ void setAllPulseLengths(uint16_t pulseValues[4])
 
 void applyToCurrentPulseValue(uint8_t servo, int8_t value)
 {
-    currentPulseValues[servo] += value;
+    float currentValue = currentPulseValues[servo];
+
+    currentValue += value * turbo / valueScales[servo];
     // clip to min max
-    if (currentPulseValues[servo] >= servo_max_pulse[servo])
+    if (currentValue >= servo_max_pulse[servo])
     {
-        currentPulseValues[servo] = servo_max_pulse[servo];
+        currentValue = servo_max_pulse[servo];
     }
 
-    if (currentPulseValues[servo] <= servo_min_pulse[servo])
+    if (currentValue <= servo_min_pulse[servo])
     {
-        currentPulseValues[servo] = servo_min_pulse[servo];
+        currentValue = servo_min_pulse[servo];
     }
+
+    currentPulseValues[servo] = currentValue;
 }
 
 void loop() 
 {
     readValuesPS2X();
 
-    int8_t left = getAverageHistoryValue(leftValues);
-    int8_t right = getAverageHistoryValue(rightValues);
-    int8_t base = getAverageHistoryValue(baseValues);
-    int8_t claw = getAverageHistoryValue(clawValues);
+    float left = getAverageHistoryValue(leftValues);
+    float right = getAverageHistoryValue(rightValues);
+    float base = getAverageHistoryValue(baseValues);
+    float claw = getAverageHistoryValue(clawValues);
     
     applyToCurrentPulseValue(LEFT_SERVO, left);
     applyToCurrentPulseValue(RIGHT_SERVO, right);
     applyToCurrentPulseValue(BASE_SERVO, base);
     applyToCurrentPulseValue(CLAW_SERVO, claw);
 
-    //Serial.println(currentPulseValues[RIGHT_SERVO]);
+    uint16_t pulseValues[4] = { 0, 0, 0, 0 };
+    for (uint8_t servo = 0; servo < 4; ++servo)
+    {
+        pulseValues[servo] = (uint16_t)currentPulseValues[servo];
+    }
 
-    setAllPulseLengths(currentPulseValues);
+    //Serial.print("L");
+    //Serial.print(currentPulseValues[LEFT_SERVO]);
+    //Serial.print(", R");
+    //Serial.print(currentPulseValues[RIGHT_SERVO]);
+    //Serial.print(", ");
+
+    //Serial.print("B");
+    //Serial.print(currentPulseValues[BASE_SERVO]);
+   // Serial.print(",");
+   // Serial.println(pulseValues[BASE_SERVO]);
+
+    setAllPulseLengths(pulseValues);
 
   //servo_setMin(LEFT_SERVO_NUM);
   //delay(5000);
